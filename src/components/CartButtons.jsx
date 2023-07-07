5b0b71f39d5138b82fbdfba9c6150b8f57790936
sansk <iamsangyk@gmail.com>
2023-07-07 11:38:13 +0530

    Stripe Checkout Setup

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -8,7 +8,7 @@ import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
-  const { totalItems } = useCartContext();
+  const { totalItems, clearCart } = useCartContext();
   const { loginWithRedirect, logout, myUser } = useUserContext();
 
   return (
@@ -24,7 +24,11 @@ const CartButtons = () => {
         <button
           className="auth-btn"
           type="button"
-          onClick={() => logout({ returnTo: window.location.origin })}>
+          onClick={() => {
+            localStorage.removeItem("user");
+            clearCart();
+            logout({ returnTo: window.location.origin });
+          }}>
           Logout <FaUserMinus />
         </button>
       ) : (
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -6,11 +6,28 @@ import { useCartContext } from "../context/cart_context";
 import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
+
   return (
     <main>
       <PageHero title="Checkout" />
-      <CheckoutContainer className="page">
-        <h1>Checkout Here!</h1>
+      <CheckoutContainer
+        className="page"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        {cart.length < 1 ? (
+          <div className="empty" style={{ textAlign: "center" }}>
+            <h2>Your Cart is Empty</h2>
+            <Link to="/products" className="btn">
+              Fill the Cart
+            </Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
       </CheckoutContainer>
     </main>
   );
