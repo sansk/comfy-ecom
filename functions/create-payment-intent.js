9cd9ff5284cb1e04202a176dfe5e5bbf4864c93a
sansk <iamsangyk@gmail.com>
2023-07-08 11:24:48 +0530

    Stripe Checkout Completed

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,39 @@
+// => domain.com/.netlify/functions/create-payment-intent
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.VITE_STRIPE_SECRET_KEY);
+
+export async function handler(event, context) {
+    if (event.body) {
+        const { cart, shippingFee, totalAmount } = JSON.parse(event.body);
+
+        const calculateOrderAmount = () => shippingFee + totalAmount;
+
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: calculateOrderAmount(),
+                currency: 'usd',
+                description: 'ECommerce Services - Comfy Furniture Payment',
+                shipping: {
+                    name: 'Jenny Rosen',
+                    address: {
+                        line1: '510 Townsend St',
+                        postal_code: '98140',
+                        city: 'San Francisco',
+                        state: 'CA',
+                        country: 'US',
+                    },
+                },
+            })
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: error.message }),
+            }
+        }
+    }
+}
--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -11,16 +11,154 @@ import axios from "axios";
 import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
 import { formatPrice } from "../utils/helpers";
-// import { useHistory } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
-  return <h4>hello from Stripe Checkout </h4>;
+  const { cart, totalAmount, shippingFee, clearCart } = useCartContext();
+  const { myUser } = useUserContext();
+  const navigate = useNavigate();
+
+  //From Stripe
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
+        },
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a",
+      },
+    },
+  };
+
+  const createPaymentIntent = async () => {
+    try {
+      const { data } = await axios.post(
+        "/.netlify/functions/create-payment-intent",
+        JSON.stringify({ cart, shippingFee, totalAmount })
+      );
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      //console.log("Error: ", error.response);
+    }
+  };
+
+  useEffect(() => {
+    createPaymentIntent();
+  }, []);
+
+  const handleChange = (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+
+      setTimeout(() => {
+        clearCart();
+        navigate("/");
+      }, 10000);
+    }
+  };
+
+  return (
+    <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank You!</h4>
+          <h4>Your Payment was Successfull!</h4>
+          <h4>Redirecting to the Home Page</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser?.name}</h4>
+          <p>Your Total is {formatPrice(shippingFee + totalAmount)}</p>
+          <p>Test Card Numbers:</p>
+          <p>
+            Payment succeeds: <span>4242 4242 4242 4242</span>
+          </p>
+          <p>
+            Payment requires authentication: <span> 4000 0025 0000 3155 </span>
+          </p>
+          <p>
+            Payment is declined: <span>4000 0000 0000 9995</span>
+          </p>
+        </article>
+      )}
+      <form id="payment-form" onSubmit={handleSubmit}>
+        <CardElement
+          id="card-element"
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button disabled={processing || disabled || succeeded} id="submit">
+          <span id="button-text">
+            {processing ? (
+              <div className="spinner" id="spinner"></div>
+            ) : (
+              "Pay now"
+            )}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? "result-message" : "result-message hidden"}>
+          Payment succeeded, see the result in your
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            Stripe dashboard.
+          </a>
+          Refresh the page to pay again.
+        </p>
+      </form>
+    </div>
+  );
 };
 
 const StripeCheckout = () => {
   return (
     <CheckoutContainer>
-      <CheckoutForm />
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </CheckoutContainer>
   );
 };
