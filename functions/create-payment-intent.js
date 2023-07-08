b507d53bff7f0e3983754ddc79e2add1144293c3
sansk <iamsangyk@gmail.com>
2023-07-08 17:03:15 +0530

    Env Variable update

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,7 +1,7 @@
 // => domain.com/.netlify/functions/create-payment-intent
 require('dotenv').config();
 
-const stripe = require('stripe')(process.env.VITE_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(event, context) {
     if (event.body) {
